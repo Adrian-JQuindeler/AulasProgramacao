f7f5cdafbcf57313ad1f1e72c9ab1a755ac31402
Adrian Jansen Quindeler <122066256+Adrian-JQuindeler@users.noreply.github.com>
2023-05-21 15:22:34 -0300

    Formatando texto e criando função para finalizar compra!

--- Python/Loja.py
+++ Python/Loja.py
@@ -14,6 +14,7 @@ def AdicionarItem(carrinho, listaDeProdutos):
                 break
             elif desejo in listaDeProdutos[i][0][:4]:
                 carrinho.append(listaDeProdutos[i])
+                print(f"{listaDeProdutos[i][0][3:]} Adicionado ao carrinho!\n")
                 break
         else:
                 print("Produto não encontrado.") 
@@ -33,6 +34,7 @@ def RemoverItem(carrinho):
                         break
                     if desejo in carrinho[i][0][:3]:
                         carrinho.remove(carrinho[i])
+                        print(f"{listaDeProdutos[i][0][3:]} Removido do carrinho!\n")
                         break
                 else:
                     print("Produto não encontrado no carrinho.")   
@@ -57,7 +59,19 @@ def MostrandoCompra(carrinho):
         print(f"\033[1;33;44m    {carrinho[i][0]:.<25}", end = "")
         print(f"{carrinho[i][1]:.2f}\033[m")
         total += carrinho[i][1]
-    print(f"O valor total é: {total:.2f}")
+    print(f"\033[1;33;44m    {'O valor total é:':.<24}{total:.2f}\033[m")
+
+def FinalizarCompra(CEP):
+    if len(carrinho) == 1:
+        print(f"Obrigado por comprar na Adrian's & Adrian's {nome},"
+        f"\nQualquer novidade, enviaremos para o email: {email}."
+        f"\nO produto chegará no CEP {CEP[:1]}.{CEP[2:5]}-{CEP[-3:]} em até 30 dias."
+        "\nAgradecemos sua visita.")
+    else:
+        print(f"Obrigado por comprar na Adrian's & Adrian's {nome},"
+        f"\nQualquer novidade, enviaremos para o email: {email}."
+        f"\nOs produtos chegaram no CEP {CEP[:1]}.{CEP[2:5]}-{CEP[-3:]} em até 30 dias."
+        "\nAgradecemos sua visita.")
 
 #Criando a lista com os produtos vendidos
 listaDeProdutos = [
@@ -75,8 +89,11 @@ print("\033[1;33;44m     Adrian's & Adrian's LTDA.     \033[m")
 print("\033[1;33;44m===================================\033[m")
 
 carrinho = []
+
+nome = input("Informe seu nome: ")
+email = input("Seu email: ")
 while True:
-    caminho = int(input("\nO que quer fazer?"
+    caminho = int(input("\nO que deseja fazer?"
                     "\n[1] Ver a lista de produtos"
                     "\n[2] Adicionar item ao carrinho"
                     "\n[3] Remover item do carrinho"
@@ -100,18 +117,20 @@ while True:
         MostrarCarrinho(carrinho)
     
     elif caminho == 5:
-        #Terminando a venda
-        MostrandoCompra(carrinho)
-        resposta = input("Deseja finalizar a compra? [S/N] ").lower()
-        
-        #Conclusão e despedida
-        if resposta == 's':
-            print("Obrigado por comprar na Adrian's & Adrian's, agradecemos sua visita.")
-            break
+        if len(carrinho) == 0:
+            print("\n\033[1;33;44m Nenhum item foi selecionado. \033[m")
         else:
-            print("Compra cancelada.")
+            #Terminando a venda
+            MostrandoCompra(carrinho)
+            resposta = input("Deseja finalizar a compra? [S/N] ").lower()
+            if resposta == "s":
+                cep = input("Informe seu CEP: ")
+                
+                #Conclusão e despedida
+                FinalizarCompra(cep)
+            else:
+                print("Compra cancelada.")
         break
-    
     else:
         print("Resposta inválida!")
-print("Volte Sempre")
+print("\033[1;33;44m Volte Sempre! \033[m")
