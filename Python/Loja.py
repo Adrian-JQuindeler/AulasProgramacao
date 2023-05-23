343083aeb4ba3f1a7e2bae794f6dc37252a7295e
Adrian Jansen Quindeler <122066256+Adrian-JQuindeler@users.noreply.github.com>
2023-05-22 21:26:21 -0300

    'Vendendo Ventiladores'
    Gabriel não aprovou o preço do Cooler

--- Python/Loja.py
+++ Python/Loja.py
@@ -1,8 +1,18 @@
 def MostrarProdutos(listaDeProdutos):
     print("\n\033[1;33;44m PRODUTOS: \033[m")
     for i in range(len(listaDeProdutos)):
-        print(f"\033[1;33;44m    {listaDeProdutos[i][0]:.<24}", end = "")
-        print(f"{listaDeProdutos[i][1]:.2f} \033[m")
+        print(f"\033[1;33;44m    {listaDeProdutos[i][0]:.<35}", end = "")
+        if listaDeProdutos[i][1] < 100:
+            print(f"..{listaDeProdutos[i][1]:.2f} \033[m")
+        elif listaDeProdutos[i][1] < 1000:
+            print(f".{listaDeProdutos[i][1]:.2f} \033[m")
+        else:
+            print(f"{listaDeProdutos[i][1]:.2f} \033[m")
+
+def MostrarEspecificações(Especificações):
+    print("\n\033[1;33;44m ESPECIFICAÇÕES: \033[m")
+    for i in range(len(Especificações)):
+        print(f"\033[1;33;44m    {Especificações[i]}\033[m")
 
 def AdicionarItem(carrinho, listaDeProdutos):
     print("\n\033[1;33;44m Digite o número do produto que deseja adicionar. \033[m"
@@ -14,7 +24,10 @@ def AdicionarItem(carrinho, listaDeProdutos):
                 break
             elif desejo in listaDeProdutos[i][0][:4]:
                 carrinho.append(listaDeProdutos[i])
-                print(f"{listaDeProdutos[i][0][3:]} Adicionado ao carrinho!\n")
+                if i < 10:
+                    print(f"{listaDeProdutos[i][0][4:]} Adicionado ao carrinho!\n")
+                elif i >= 10:
+                    print(f"{listaDeProdutos[i][0][5:]} Adicionado ao carrinho!\n")
                 break
         else:
                 print("Produto não encontrado.") 
@@ -48,18 +61,34 @@ def MostrarCarrinho(carrinho):
         print("\n\033[1;33;44m PRODUTOS ESCOLHIDOS: \033[m")
         carrinho.sort()
         for i in range(len(carrinho)):
-            print(f"\033[1;33;44m    {carrinho[i][0]:.<24}", end = "")
-            print(f"{carrinho[i][1]:.2f} \033[m")
+            print(f"\033[1;33;44m    {carrinho[i][0]:.<35}", end = "")
+            if carrinho[i][1] < 100:
+                print(f"..{carrinho[i][1]:.2f} \033[m")
+            elif carrinho[i][1] < 1000:
+                print(f".{carrinho[i][1]:.2f} \033[m")
+            else:
+                print(f"{carrinho[i][1]:.2f} \033[m")
 
 def MostrandoCompra(carrinho):
     total = 0
     carrinho.sort()
     print(f"\033[1;33;44m Seu carrinho: \033[m")
     for i in range(len(carrinho)):
-        print(f"\033[1;33;44m    {carrinho[i][0]:.<25}", end = "")
-        print(f"{carrinho[i][1]:.2f}\033[m")
+        print(f"\033[1;33;44m    {carrinho[i][0]:.<35}", end = "")
+        if carrinho[i][1] < 100:
+            print(f"..{carrinho[i][1]:.2f} \033[m")
+        elif carrinho[i][1] < 1000:
+            print(f".{carrinho[i][1]:.2f} \033[m")
+        else:
+            print(f"{carrinho[i][1]:.2f} \033[m")
         total += carrinho[i][1]
-    print(f"\033[1;33;44m    {'O valor total é:':.<24}{total:.2f}\033[m")
+    print(f"\033[1;33;44m    {'O valor total é:':.<35}", end = "")
+    if total < 100:
+        print(f"..{total:.2f} \033[m")
+    elif total < 1000:
+        print(f".{total:.2f} \033[m")
+    else:
+        print(f"{total:.2f} \033[m")
 
 def FinalizarCompra(CEP):
     if len(carrinho) == 1:
@@ -75,13 +104,31 @@ def FinalizarCompra(CEP):
 
 #Criando a lista com os produtos vendidos
 listaDeProdutos = [
-    ["[1] PLACA-MÃE", 956.81],
+    ["[1] PLACA-MÃE", 621.81],
     ["[2] PROCESSADOR", 729.99],
-    ["[3] SSD", 649.00],
+    ["[3] SSD", 249.00],
     ["[4] MEMÓRIA RAM 16GB", 314.25],
     ["[5] GABINETE", 135.46],
-    ["[6] COOLER", 159.69],
-    ["[7] MONITOR", 861.47]]
+    ["[6] COOLER", 55.99],
+    ["[7] MONITOR", 761.47],
+    ["[8] FONTE", 98.25],
+    ["[9] MOUSE", 115.50],
+    ["[10] MOUSE PAD", 22.13],
+    ["[11] COOLER + PROCESSADOR + SSD", 999.99],
+    ["[12] PLACA-MÃE + MEMÓRIA RAM", 815.20]]
+
+Especificações = ["Placa-Mãe Asus Prime A520M-E. ",
+    "Processador ",
+    "SSD",
+    "Memória RAM 16gb",
+    "Gabinete",
+    "Cooler",
+    "Monitor",
+    "Fonte",
+    "MOUSE",
+    "Mouse pad",
+    "Cooler + Processador + SSD",
+    "Placa-Mãe + Memória RAM"]
 
 #Mostrando o nome da empresa
 print("\033[1;33;44m===================================\033[m")
@@ -95,28 +142,33 @@ email = input("Seu email: ")
 while True:
     caminho = int(input("\nO que deseja fazer?"
                     "\n[1] Ver a lista de produtos"
-                    "\n[2] Adicionar item ao carrinho"
-                    "\n[3] Remover item do carrinho"
-                    "\n[4] Mostrar carrinho"
-                    "\n[5] Finalizar compra"
+                    "\n[2] Mostrar especificações"
+                    "\n[3] Adicionar item ao carrinho"
+                    "\n[4] Remover item do carrinho"
+                    "\n[5] Mostrar carrinho"
+                    "\n[6] Finalizar compra"
                     "\nSua resposta: "))
     if caminho == 1:
         #Mostrando os produtos de forma organizada
         MostrarProdutos(listaDeProdutos)
-        
+    
     elif caminho == 2:
+        #Mostrando especificações dos produtos
+        MostrarEspecificações(Especificações)
+        
+    elif caminho == 3:
         #Colocando produtos no carrinho do cliente
         AdicionarItem(carrinho, listaDeProdutos)
     
-    elif caminho == 3:
+    elif caminho == 4:
         #Removendo produtos no carrinho do cliente
         RemoverItem(carrinho)    
 
-    elif caminho == 4:
+    elif caminho == 5:
         #Mostrando o carrinho do cliente
         MostrarCarrinho(carrinho)
     
-    elif caminho == 5:
+    elif caminho == 6:
         if len(carrinho) == 0:
             print("\n\033[1;33;44m Nenhum item foi selecionado. \033[m")
         else:
